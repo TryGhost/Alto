a648321e65c565992d65caaf85b71fe1f1aec98f
IVEEL <admin@iveel.co>
2019-06-13 10:00:01 +0800

    Version 1.1.1

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ const concat = require('gulp-concat');
 const exec = require('child_process').exec;
 
 const theme = 'alto';
-const current = '1.0.0';
-const next = '1.1.0';
+const current = '1.1.0';
+const next = '1.1.1';
 const root = '/Users/sodbileg/Developer/ghost-themes';
 const final = '/Users/sodbileg/Dropbox/IVEEL/Alto';
 
