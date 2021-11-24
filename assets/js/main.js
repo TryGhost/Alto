7a04cbd2a355858154971af4712e197bdb7f2cc0
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2021-11-24 10:28:57 +0800

    Remove bookmark and gallery card assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@ $(function () {
     whiteLogo();
     carousel();
     video();
-    gallery();
     author();
     offCanvas();
 });
@@ -76,17 +75,6 @@ function carousel() {
 function video() {
     'use strict';
     $('.post-content').fitVids();
-}
-
-function gallery() {
-    var images = document.querySelectorAll('.kg-gallery-image img');
-    images.forEach(function (image) {
-        var container = image.closest('.kg-gallery-image');
-        var width = image.attributes.width.value;
-        var height = image.attributes.height.value;
-        var ratio = width / height;
-        container.style.flex = ratio + ' 1 0%';
-    });
 }
 
 function author() {
